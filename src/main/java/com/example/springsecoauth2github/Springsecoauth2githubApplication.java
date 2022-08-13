13a1cf03e5f3d8ed132f5288a6d34ffe200ee649
Arun Yadava <arunrnyada@gmail.com>
2022-08-13 17:27:50 +0530

    Initial commit

---
+++ src/main/java/com/example/springsecoauth2github/Springsecoauth2githubApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecoauth2github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecoauth2githubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecoauth2githubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecoauth2github/Springsecoauth2githubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecoauth2github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecoauth2githubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
